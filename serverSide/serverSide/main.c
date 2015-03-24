dcfb3ec712bffe05572f0fb37b350bd0003dd966
molak <molakttj@gmail.com>
2015-03-24 17:34:03 +0100

    new

---
+++ serverSide/serverSide/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  serverSide
+//
+//  Created by Andrzej Lesniak on 24.03.2015.
+//  Copyright (c) 2015 Michał Leśniak. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
